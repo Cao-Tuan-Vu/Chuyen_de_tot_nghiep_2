fe6b6edd38ebacc1e1f34db058e619e1e7f7f299
phucanh-design <phucanh-design@users.noreply.github.com>
2026-04-23 16:19:14 +0700

    update code

---
+++ test/widget_test.dart
@@ -0,0 +1,15 @@
+// This is a basic widget test file.
+//
+// If this code is executed, it will fail with:
+// LateInitializationError: Field '_instance@106780421' has not been initialized.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // You can replace this with your actual widget test
+    expect(true, true);
+  });
+}
+
